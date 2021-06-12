b854a16511d266c3c540eaf6b265ba7c103e73a3
Ananthvasanth <ananthakumar447@gmail.com>
2021-06-12 13:14:10 +0530

    Create area of a circle.c

---
+++ c/area of a circle.c
@@ -0,0 +1,18 @@
+//5. Program to accept value of radius and print area of a circle.
+
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+  float area,radius;
+
+  printf("Enter Radius:");
+  scanf("%f",&radius);
+
+  area=3.14*radius*radius;
+
+  printf("Area : %6.2f",area);
+
+  getch();
+}
