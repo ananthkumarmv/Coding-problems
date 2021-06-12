5cead76c1806a47a689e0fb29bd26f05705b5693
Ananthvasanth <ananthakumar447@gmail.com>
2021-06-12 13:12:14 +0530

    Create XOR of XOR's of all subsets.cpp

---
+++ geeksforgeeks/XOR of XOR's of all subsets.cpp
@@ -0,0 +1,25 @@
+
+// C++ program to find XOR of XOR's of all subsets
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// Returns XOR of all XOR's of given subset
+int findXOR(int Set[], int n)
+{
+    // XOR is 1 only when n is 1, else 0
+    if (n == 1)
+       return Set[0];
+    else
+       return 0;
+}
+ 
+// Driver program
+int main()
+{
+    int Set[] = {1,5,6};
+    int n = sizeof(Set)/sizeof(Set[0]);
+    cout << "XOR of XOR's of all subsets is "
+         << findXOR(Set, n);
+    return 0;
+}
+
