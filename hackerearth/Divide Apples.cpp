aae156e033c95e26a0a59f6ec056d4bc237236d7
Ananthvasanth <ananthakumar447@gmail.com>
2021-06-12 13:12:07 +0530

    Create Divide Apples.cpp

---
+++ hackerearth/Divide Apples.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<algorithm>
+#define FIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define z int
+#define g string
+#define f(n) for(z i=0;i<n;i++)
+#define ll long long
+
+using namespace std;
+
+int main() {
+    FIO;
+    ll z n;
+    cin>>n;
+    z a[n];
+    f(n){
+    	cin>>a[i];
+	}
+    
+    
+    return 0;
+}
+
+
