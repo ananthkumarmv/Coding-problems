1e2c26c2572f0953c173454c36cb22b9e03b42f0
Ananthvasanth <ananthakumar447@gmail.com>
2021-06-12 13:13:42 +0530

    Create Vowel or not.c

---
+++ c/Vowel or not.c
@@ -0,0 +1,15 @@
+//Program to take an alphabet from user and check whether it is a vowel or not.
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char ch;
+    printf("Enter an alphabet : ");
+    scanf("%c", &ch);
+
+    if (ch=='a' || ch=='A' || ch=='e' || ch=='E' || ch=='i' || ch=='I' || ch=='o' || ch=='O' || ch=='u' || ch=='U')
+        printf("%c is a vowel.", ch);
+    else
+        printf("%c is not a vowel.", ch);
+}
