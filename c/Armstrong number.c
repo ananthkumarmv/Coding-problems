2f0161773dacdcfd634df7ba13fa04d444723214
Ananthvasanth <ananthakumar447@gmail.com>
2021-06-12 13:14:13 +0530

    Create Armstrong number.c

---
+++ c/Armstrong number.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    int i=2, temp, rem, sum=0, n;
+
+    printf("Enter n : ");
+    scanf("%d", &n);
+    temp = n;
+
+    while(n>0)
+    {
+        rem = n%10;
+        sum = sum+(rem*rem*rem);
+        n = n/10;
+    }
+
+    if(temp == sum)
+        printf("Entered number is an Armstrong Number");
+    else
+        printf("Entered number is not an Armstrong Number");
+
+}
