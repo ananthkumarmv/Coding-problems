f6270d733099f9b6819c7cdd1cc20fdf0282eefd
Ananthvasanth <ananthakumar447@gmail.com>
2021-06-12 13:13:38 +0530

    Create Theatre-Square.cpp

---
+++ codeforces/Theatre-Square.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+typedef long long ll;
+using namespace std;
+int main(){
+	ll n,m,a,len,bre;
+	cin>>n>>m>>a;
+	len = n/a;
+	if(n%a!=0) len++;
+	bre=m/a;
+	if(m%a!=0) bre++;
+	cout<<len*bre;
+
+	return 0;
+}
+
