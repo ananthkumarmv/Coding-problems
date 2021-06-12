a768e718452f72ba7594d9c12edf5e535c5461fe
Ananthvasanth <ananthakumar447@gmail.com>
2021-06-12 13:14:18 +0530

    Create Leapyear.cpp

---
+++ others/Leapyear.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int y;
+	cin>>y;
+	if((y%4==0)&&(y%100!=0)||(y%400==0))
+		cout<<"Leap Year";
+
+	else 
+	    cout<<"Not a Leap Year";
+	
+}
