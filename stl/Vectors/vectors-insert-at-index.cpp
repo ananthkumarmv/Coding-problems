2a140789a38b5a49fbc661ea4af46db9ec362f76
Ananthvasanth <ananthakumar447@gmail.com>
2021-06-12 13:09:13 +0530

    Create vectors-insert-at-index.cpp

---
+++ stl/Vectors/vectors-insert-at-index.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include <vector>
+using namespace std;
+
+void print(const vector<int>& my_vector){
+	cout<<"Vector content: { ";
+	for(int i=0; i<my_vector.size();i++)
+		cout<<my_vector[i]<<" ";
+	cout<<"}\n\n";
+}
+
+
+int main(){
+	vector<int> numbers;
+	int value=0;
+	
+	cout<<"Pushing back...\n";
+	while(value>=0){
+		cout<<"Enter number: ";
+		cin>>value;
+		if(value>=0)
+			numbers.push_back(value);
+	} 
+	print(numbers);
+	
+	int index=0;
+	cout<<"Inserting by index...\n";
+	while(index>=0){
+		cout<<"Enter index: ";
+		cin>>index;
+		if(index>=0){
+			cout<<"Enter value: ";
+			cin>>value;
+			numbers.insert(numbers.begin()+index,value);
+		}
+			
+	} 
+	print(numbers);
+
+	return 0;
+}
+
