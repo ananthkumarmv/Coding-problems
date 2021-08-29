4f51fefc2baf4c1fec6eea34d52bb2637c091ed2
Anant <mvananthkumar@gmail.com>
2021-08-29 23:25:03 +0530

    Committed

---
+++ others/pattern3.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+
+using namespace std;
+
+void printABCDPattern(int n){
+	char ch;
+	
+	for(int i=1;i<=n;i++){
+		ch='A';
+		for(int j=1; j<=n-i+1;j++){
+			cout<<ch;
+			ch++;
+		}
+		cout<<endl;
+	}
+}
+
+int main(){
+	int n;
+	cin>>n;
+	printABCDPattern(n);
+
+	return 0;
+}
+
