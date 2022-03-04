31693f1ccfc002673ea00e427cdc19e6e01e81d0
Ananth <mvananthkumar@gmail.com>
2022-03-04 16:22:30 +0530

    Create Count-special-char.cpp

---
+++ others/Count-special-char.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+	
+	string str;
+	getline(cin, str);
+	int i=0,special=0;
+	while(str[i] != '\0') 
+    { 
+    	if((isalpha(str[i])==0) && (isdigit(str[i])==0) && (isspace(str[i])==0)) special++;    	 
+    	i++; 
+	} 
+	cout<<special;
+    return 0;
+}
+
+
