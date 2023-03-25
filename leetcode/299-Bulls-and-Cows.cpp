0f769bdf31e057b5c438fcd284ba8a61cf3ceb00
Ananth <mvananthkumar@gmail.com>
2023-03-25 17:56:47 +0530

    Initial commit

---
+++ leetcode/299-Bulls-and-Cows.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string getHint(string secret, string guess) {
+
+        int n = secret.length();
+
+        int bulls = 0;
+        int cows = 0;
+
+        vector<int> s(10, 0);
+        vector<int> g(10, 0);
+
+        for(int i=0; i<n; i++){
+            if(secret[i] == guess[i]){
+                bulls++;
+            }
+            else{
+                s[secret[i]-'0']++;
+                g[guess[i]-'0']++;
+            }
+        }
+
+        for(int i=0; i<10; i++){
+            cows += min(s[i], g[i]);
+        }
+
+        string res = to_string(bulls) + 'A' + to_string(cows) + 'B';
+
+        return res;
+
+    }
+};
